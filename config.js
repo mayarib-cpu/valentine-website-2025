d438127b7a19b84e9c7b8c540475b2e2b3666f9f
mayarib-cpu <discardmailk@gmail.com>
2025-02-11 21:11:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dsn5fb7vl/video/upload/v1739288259/spotidownloader.com_-_Wear_Sunscreen_-_Peach_Pyramid_nbuks6.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
